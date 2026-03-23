58acd99cf2b686ee3d629b5b1948614fd1fd05fb
Nguyen Chidi <nguyenchidi.dev@gmail.com>
2026-03-23 12:42:43 +0700

    feat(core): implement JWT security and authentication configuration components

---
+++ banking_core_java/src/main/java/com/chidibank/core/infrastructure/security/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.chidibank.core.infrastructure.security;
+
+import com.chidibank.core.adapter.out.persistence.entity.UserEntity;
+import com.chidibank.core.adapter.out.persistence.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
+        
+        return new User(user.getEmail(), user.getPassword(), Collections.emptyList());
+    }
+}
